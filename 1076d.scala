01fd4e786d5458cb2cc5a8548130498744390238
Clouds Flowing <clouds.flowing@gmail.com>
2018-12-28 15:54:54 +0800

    use buffered reader

--- 1076d.scala
+++ 1076d.scala
@@ -1,5 +1,33 @@
 object CF1076d extends App {
-  val in = new java.util.Scanner(System.in)
+  import java.io.{InputStream, InputStreamReader, BufferedReader}
+  import java.util.StringTokenizer
+  // https://codeforces.com/blog/entry/21074
+  class ReadScanner(reader: BufferedReader, radix: Int = 10) extends Iterator[String] with AutoCloseable {
+    def this(stream: InputStream) = this(new BufferedReader(new InputStreamReader(stream)))
+    private[this] val tokenizers = Iterator.continually(reader.readLine()).takeWhile(_ != null).map(new StringTokenizer(_))
+    private[this] var current: Option[StringTokenizer] = None
+    @inline private[this] def tokenizer(): Option[StringTokenizer] = current.find(_.hasMoreTokens) orElse {
+      current = if (tokenizers.hasNext) Some(tokenizers.next()) else None; current
+    }
+
+    override def next() = tokenizer().get.nextToken()
+    override def hasNext = tokenizer().nonEmpty
+    override def close() = reader.close()
+    def line(): String = tokenizer().get.nextToken("\n\r")
+    def nextString(): String = next()
+    def nextChar(): Char = next().ensuring(_.length == 1).head
+    def nextBoolean(): Boolean = next().toBoolean
+    def nextByte(): Byte = java.lang.Byte.parseByte(next(), radix)
+    def nextShort(): Short = java.lang.Short.parseShort(next(), radix)
+    def nextInt(): Int = java.lang.Integer.parseInt(next(), radix)
+    def nextLong(): Long = java.lang.Long.parseLong(next(), radix)
+    def nextBigInt(): BigInt = BigInt(next(), radix)
+    def nextFloat(): Float = next().toFloat
+    def nextDouble(): Double = next().toDouble
+    def nextBigDecimal(): BigDecimal = BigDecimal(next())
+  }
+  // val startTime = System.currentTimeMillis()
+  val in = new ReadScanner(System.in)
   val out = new java.io.PrintWriter(System.out)
 
   import scala.math._
@@ -61,7 +89,6 @@ object CF1076d extends App {
   }
 
   val (n, m, k) = (in.nextInt, in.nextInt, in.nextInt)
-  // val startTime = System.currentTimeMillis()
   val edges = 1 to m map ((_: Int) => {
     val (x, y, w) = (in.nextInt, in.nextInt, in.nextLong)
     (x-1, y-1, w)

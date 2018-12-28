ff7be89bb51e78b8559a0f1a05bf49b23cc0c933
Clouds Flowing <clouds.flowing@gmail.com>
2018-12-29 04:55:13 +0800

    [MLE] 47663462: test 1

--- 1076e.cpp
+++ 1076e.cpp
@@ -7,18 +7,28 @@ using namespace std;
 
 typedef vector<vector<pair<int, int>>> update_type;
 typedef vector<vector<int>> edge_type;
+typedef int node_ptr;
 
 struct node {
-  shared_ptr<node> pl, pr;
+  node_ptr pl, pr;
   int l, r;
   long long data;
-} empty_node;
+} empty_node[5*20*300020]; // M*log_2(M)*4
+// vector<node> empty_node(5*20*300020);
 
-shared_ptr<node> make_node(int l, int r) {
+node_ptr make_node(int l, int r) {
   // cerr << "making: " << l << ", " << r << endl;
-  auto head = make_shared<node>(empty_node);
-  head->l = l; head->r = r;
-  return head;
+  static int p = 1;
+  // if (p >= empty_node.size()) {
+  //   cerr << "double size" << endl;
+  //   empty_node.resize(empty_node.size()*2);
+  // }
+  empty_node[p].l = l, empty_node[p].r = r;
+  return p++;
+}
+
+node& get_node(node_ptr p) {
+  return empty_node[p];
 }
 
 void update(node& head, int left, int right, int data) {
@@ -29,14 +39,16 @@ void update(node& head, int left, int right, int data) {
   }
   int m = (head.l + head.r) / 2; // [left, m], [m+1, right]
   if (left <= m) {
-    if (!head.pl) head.pl = make_node(head.l, m);
-    head.pl = make_shared<node>(*head.pl);
-    update(*head.pl, left, right, data);
+    node_ptr pl = head.pl;
+    head.pl = make_node(head.l, m);
+    if (pl) get_node(head.pl) = get_node(pl);
+    update(get_node(head.pl), left, right, data);
   }
   if (right > m) {
-    if (!head.pr) head.pr = make_node(m+1, head.r);
-    head.pr = make_shared<node>(*head.pr);
-    update(*head.pr, left, right, data);
+    node_ptr pr = head.pr;
+    head.pr = make_node(m+1, head.r);
+    if (pr) get_node(head.pr) = get_node(pr);
+    update(get_node(head.pr), left, right, data);
   }
 }
 
@@ -44,19 +56,19 @@ long long query(const node& head, int k) {
   int m = (head.l + head.r) / 2;
   if (k <= m) {
     if (!head.pl) return head.data;
-    return head.data + query(*head.pl, k);
+    return head.data + query(get_node(head.pl), k);
   }
   if (k > m) {
     if (!head.pr) return head.data;
-    return head.data + query(*head.pr, k);
+    return head.data + query(get_node(head.pr), k);
   }
   return head.data; // assert false
 }
 
 void dump(const node& head) {
   cerr << "(" << head.l << ", " << head.r << "): " << head.data << endl;
-  if (head.pl) dump(*head.pl);
-  if (head.pr) dump(*head.pr);
+  if (head.pl) dump(get_node(head.pl));
+  if (head.pr) dump(get_node(head.pr));
 }
 
 void do_dfs(vector<long long>& result, int depth, int parent, int now, const node& prev_head,
@@ -77,8 +89,8 @@ void do_dfs(vector<long long>& result, int depth, int parent, int now, const nod
 
 vector<long long> dfs(int n, const edge_type& edges, const update_type& updates) {
   vector<long long> result(n, 0);
-  auto head = make_node(0, n);
-  do_dfs(result, 0, -1, 0, *head, edges, updates);
+  node_ptr head = make_node(0, n);
+  do_dfs(result, 0, -1, 0, get_node(head), edges, updates);
   return result;
 }
 

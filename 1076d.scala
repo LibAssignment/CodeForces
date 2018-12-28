0aaf2a6169366a6a00999fdb69ea8738b96dd3e5
Clouds Flowing <clouds.flowing@gmail.com>
2018-12-28 16:25:08 +0800

    remove order

--- 1076d.scala
+++ 1076d.scala
@@ -26,38 +26,37 @@ object CF1076d extends App {
     def nextDouble(): Double = next().toDouble
     def nextBigDecimal(): BigDecimal = BigDecimal(next())
   }
-  // val startTime = System.currentTimeMillis()
+  val startTime = System.currentTimeMillis()
   val in = new ReadScanner(System.in)
   val out = new java.io.PrintWriter(System.out)
 
   import scala.math._
   import scala.collection.mutable.{Queue, HashSet, HashMap, ListBuffer}
 
-  def edgeToList(edges: Seq[(Int, Int, Long)], direction: Boolean = false) : Array[List[(Int, Long)]] = {
-    val acc = Array.fill[List[(Int, Long)]](n)(List())
-    edges foreach ((z) => {
-      val (x,y,w) = z
-      acc(x) = (y, w)::acc(x)
+  def edgeToList(edges: Seq[(Int, Int, Long, Int)], direction: Boolean = false) : Seq[ListBuffer[(Int, Long, Int)]] = {
+    val acc = Array.fill(n)(ListBuffer[(Int, Long, Int)]())
+    edges foreach ({case (x,y,w,i) =>
+      acc(x) += ((y, w, i))
       if (!direction) {
-        acc(y) = (x, w)::acc(y)
+        acc(y) += ((x, w, i))
       }
     })
-    acc
+    acc// map (_.toList)
   }
 
   // if k <= n, exactly k point would be good
   // construct a tree that is minimum (respect to path) spanning tree
   // or shortest path to all node
-  def shortest(n: Int, edges: Seq[List[(Int, Long)]]) : Array[(Int, Long)] = {
-    val result = Array.fill[(Int, Long)](n)((-1, Long.MaxValue))
-    result(0) = (0, 0);
+  def shortest(n: Int, edges: Seq[ListBuffer[(Int, Long, Int)]]) : Array[(Int, Long, Int)] = {
+    val result = Array.fill[(Int, Long, Int)](n)((-1, Long.MaxValue, 0))
+    result(0) = (0, 0, 0);
     val (queue, set) = (Queue(0), HashSet(0))
     while (queue.nonEmpty) {
       val x = queue.dequeue
       set remove x
-      edges(x) foreach ({case (y, w) =>
+      edges(x) foreach ({case (y, w, i) =>
         if (result(x)._2 + w < result(y)._2) {
-          result(y) = (x, result(x)._2 + w)
+          result(y) = (x, result(x)._2 + w, i)
           if (set add y) {
             queue enqueue y
           }
@@ -67,52 +66,41 @@ object CF1076d extends App {
     result
   }
 
-  def bfs(tree: Seq[(Int, Long)], k: Int) : List[(Int, Int)] = {
-    val nodeList = edgeToList(tree.zipWithIndex.drop(1).map({case ((y, w), x) => (y, x, w)}), true)
+  def bfs(tree: Seq[(Int, Long, Int)], k: Int) : List[(Int, Int, Int)] = {
+    val nodeList = edgeToList(tree.zipWithIndex.drop(1).map({case ((y, w, i), x) => (y, x, w, i)}), true)
     // nodeList foreach println
     val queue = Queue(0)
-    var result = ListBuffer[(Int, Int)]()
+    var result = ListBuffer[(Int, Int, Int)]()
     while (queue.nonEmpty && result.length < k) {
       val x = queue.dequeue
       queue ++= nodeList(x).map(_._1)
-      result ++= nodeList(x).map((z) => {(z._1, x)})
+      result ++= nodeList(x).map({case (y, _, i) => (y, x, i)})
     }
     return result.toList.take(k);
   }
 
-  def orderBy(edges: Seq[(Int, Int)], allEdges: Seq[(Int, Int, Long)]) : List[Int] = {
-    val map = HashMap[(Int, Int), Int]()
-    allEdges.zipWithIndex.foreach({
-      case ((x, y, _), i) => map += ((x, y) -> i); map += ((y, x) -> i)
-    })
-    edges.map(map(_)).toList.sorted
-  }
-
   val (n, m, k) = (in.nextInt, in.nextInt, in.nextInt)
-  val edges = 1 to m map ((_: Int) => {
+  val edges = 1 to m map ((i) => {
     val (x, y, w) = (in.nextInt, in.nextInt, in.nextLong)
-    (x-1, y-1, w)
+    (x-1, y-1, w, i)
   })
-  // println("edges:", System.currentTimeMillis()-startTime)
+  println("edges:", System.currentTimeMillis()-startTime)
   // edges foreach (println(_))
 
   val nodeEdges = edgeToList(edges)
-  // println("nodeEdges:", System.currentTimeMillis()-startTime)
+  println("nodeEdges:", System.currentTimeMillis()-startTime)
   // nodeEdges foreach (println(_))
 
   val tree = shortest(n, nodeEdges)
-  // println("spfa:", System.currentTimeMillis()-startTime)
+  println("spfa:", System.currentTimeMillis()-startTime)
   // tree foreach println
 
   val remainEdges = bfs(tree, k)
-  // println("bfs:", System.currentTimeMillis()-startTime)
+  println("bfs:", System.currentTimeMillis()-startTime)
   // remainEdges foreach println
 
-  val result = orderBy(remainEdges, edges)
-  // println("order:", System.currentTimeMillis()-startTime)
-
-  out.println(result.length)
-  out.println(result.map(_+1).mkString(" "))
-  // println("mkString:", System.currentTimeMillis()-startTime)
+  out.println(remainEdges.length)
+  out.println(remainEdges.map(_._3).mkString(" ").length)
+  println("mkString:", System.currentTimeMillis()-startTime)
   out.flush;out.close;
 }
